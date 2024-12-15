c98ce7ebdd5721c715c22d4215f5c6614ccf5e9f
Ariel <arielrm@gmail.com>
2024-12-14 23:02:19 -0300

    fix

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,20 +1,20 @@
 import { RefObject } from "react"
 import {
-  NewSessionData,
-  NewSessionRequestQualityEnum,
   StreamingAvatarApi,
+  type StartAvatarResponse,
+  type AvatarQuality,
 } from "@heygen/streaming-avatar"
 import { atom } from "jotai"
 
 //Stream Atoms
-export const mediaStreamActiveAtom = atom<Boolean>(false)
-export const sessionDataAtom = atom<NewSessionData | undefined>(undefined)
+export const mediaStreamActiveAtom = atom<boolean>(false)
+export const sessionDataAtom = atom<StartAvatarResponse | undefined>(undefined)
 export const streamAtom = atom<MediaStream | undefined>(undefined)
 export const debugAtom = atom<string>("")
 export const inputTextAtom = atom<string>("")
 export const avatarIdAtom = atom<string>("")
 export const voiceIdAtom = atom<string>("")
-export const qualityAtom = atom<NewSessionRequestQualityEnum>("medium")
+export const qualityAtom = atom<AvatarQuality>("medium")
 export const avatarAtom = atom<RefObject<StreamingAvatarApi> | undefined>(undefined)
 
 // Video Processing Atoms
