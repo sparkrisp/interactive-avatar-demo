b6ab705e00e4e806eccd6a0acbf33c1c0c55d20b
Ariel <arielrm@gmail.com>
2025-03-12 23:03:15 -0300

    probando otro avatar, que el chino se puso la gorra

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,9 +1,14 @@
 export const AVATARS = [
-  {
+  /*{
     id: 'Wayne_20240711',
     name: 'Enólogo Profesional',
     avatar_id: 'Wayne_20240711'
-  }
+  }*/
+    {
+      id: '2c57ba04ef4d4a5ca30a953d0791e7e3',
+      name: 'Milton - Enólogo Profesional',
+      avatar_id: '2c57ba04ef4d4a5ca30a953d0791e7e3'
+    }
 ];
 
 export const STT_LANGUAGE_LIST = [
